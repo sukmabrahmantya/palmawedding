74cdc76bea9b71b549f9962939fbccdc4948e953
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-06 16:51:18 +0700

    adding testimony models in server side

--- server/models/testimoni.js
+++ server/models/testimoni.js
@@ -3,6 +3,22 @@
 const { Schema, model } = require('mongoose')
 
 const testimoniSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Client Name cannot be empty!']
+  },
+  date: {
+    name: {
+      type: Date,
+      required: [true, 'Wedding Date cannot be empty!']
+    },
+  },
+  testimoni: {
+    name: {
+      type: String,
+      required: [true, 'Testimony cannot be empty!']
+    },
+  }
 })
 
 const Testimoni = model('Testimoni', testimoniSchema)
